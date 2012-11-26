6b34b2c45ae5c8855fc07cc8551551d904ec52a2
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-11-26 12:25:50 +0100

    minor change

--- SciVis2012/source/Visualization.h
+++ SciVis2012/source/Visualization.h
@@ -5,49 +5,49 @@ class Simulation;
 
 class Visualization
 {
-	friend class Application;
-	
-	public:
-		
-		Visualization();
-		void visualize(Simulation const &simulation, int winWidth, int winHeight);
-		
-		//different types of color mapping: black-and-white, rainbow, banded
-		enum ColorMode
-		{
-                    Grayscale,
-                    Rainbow,
-                    Custom,
-                    ColorModeCount				// (automatically assigned)
-		};
-		
-		enum Option
-		{
-			DrawSmoke,				// draw the smoke or not
-			DrawVectorField,		// draw the vector field or not
-			UseDirectionColoring,	// use direction color-coding or not
-			OptionsCount			// (automatically assigned)
-		};
-		
-                void setScalarCol(ColorMode colorMode);
-		void toggle(Option option);
-		void enable(Option option);
-		void disable(Option option);
-		bool is_enabled(Option option) const;		
-		void scale_hedgehogs(float scale);
-		float hedgehog_scale() const;
-		
-		void rainbow(float value, float* R, float* G, float* B);
-                void grayscale(float value, float* R, float* G, float* B);
-                void custom(float value, float* R, float* G, float* B);
-		void set_colormap(float vy);
-		void direction_to_color(float x, float y, int method);
-		
-	private:
-		
-		float vec_scale;				// scaling of hedgehogs 
-		int options[OptionsCount];		// options boolean array
-		ColorMode scalar_col;			//method for scalar coloring
+    friend class Application;
+
+    public:
+
+        Visualization();
+        void visualize(Simulation const &simulation, int winWidth, int winHeight);
+
+        //different types of color mapping: black-and-white, rainbow, banded
+        enum ColorMode
+        {
+            Grayscale,
+            Rainbow,
+            Custom,
+            ColorModeCount				// (automatically assigned)
+        };
+
+        enum Option
+        {
+                DrawSmoke,				// draw the smoke or not
+                DrawVectorField,		// draw the vector field or not
+                UseDirectionColoring,	// use direction color-coding or not
+                OptionsCount			// (automatically assigned)
+        };
+
+        void setScalarCol(ColorMode colorMode);
+        void toggle(Option option);
+        void enable(Option option);
+        void disable(Option option);
+        bool is_enabled(Option option) const;		
+        void scale_hedgehogs(float scale);
+        float hedgehog_scale() const;
+
+        void rainbow(float value, float* R, float* G, float* B);
+        void grayscale(float value, float* R, float* G, float* B);
+        void custom(float value, float* R, float* G, float* B);
+        void set_colormap(float vy);
+        void direction_to_color(float x, float y, int method);
+
+    private:
+
+        float vec_scale;				// scaling of hedgehogs 
+        int options[OptionsCount];		// options boolean array
+        ColorMode scalar_col;			//method for scalar coloring
 };
 
 #endif
